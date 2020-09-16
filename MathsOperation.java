e4ac1df8f70cab2e7e1d6ffecd56fb99e009e1c3
Jatin Sharma <jatin@testautomationacademy.in>
2020-09-16 21:37:35 +0530

    added line 5 ename=Vijay

--- MathsOperation.java
+++ MathsOperation.java
@@ -2,7 +2,7 @@ public class MathsOperation{
 public static void main(String args[]){
 int number1,number2;
 int result;
-
+String eName="Vijay";
 result=number1*number2; 
 Sysout(result); 
 result=number1-number2;
