4b2c35157c5c1a25a01b997276395bca2d6e515c
Jatin Sharma <jatin@testautomationacademy.in>
2020-09-16 21:35:21 +0530

    added line number5 name=nandita

--- MathsOperation.java
+++ MathsOperation.java
@@ -2,7 +2,7 @@ public class MathsOperation{
 public static void main(String args[]){
 int number1,number2;
 int result;
-
+String name="nandita";
 result=number1*number2; 
 Sysout(result); 
 result=number1-number2;
